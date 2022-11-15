503c066c3ecaa8566a90555354f5d8cf5f265191
Daria Gortchacow <daria.gortchacow@gmail.com>
2022-11-15 16:51:18 +0100

    stating

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  new_title = Faker::JapaneseMedia::StudioGhibli.movie
+  new_content = Faker::JapaneseMedia::StudioGhibli.quote
+  new_article = Article.new(title: new_title, content: new_content)
+  new_article.save
+end
